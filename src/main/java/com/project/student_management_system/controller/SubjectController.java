0e6c746d679aca6b41bed0dd5a7a92a7e01d3d8c
AbhishekRH <abhishek7.rh@gmail.com>
2024-06-22 18:31:40 +0530

    second commit

--- src/main/java/com/project/student_management_system/controller/SubjectController.java
+++ src/main/java/com/project/student_management_system/controller/SubjectController.java
@@ -1,38 +1,3 @@
-//package com.project.student_management_system.controller;
-//
-//import com.project.student_management_system.model.Subject;
-//import com.project.student_management_system.repository.SubjectRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/subjects")
-//@CrossOrigin(origins = "http://localhost:3000")
-//public class SubjectController {
-//
-//    @Autowired
-//    private SubjectRepository subjectRepository;
-//
-//    @GetMapping
-//    public List<Subject> getAllSubjects() {
-//        return subjectRepository.findAll();
-//    }
-//
-//    @PostMapping
-//    public Subject createSubject(@RequestBody Subject subject) {
-//        return subjectRepository.save(subject);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteSubject(@PathVariable String id) {
-//        subjectRepository.deleteById(id);
-//    }
-//}
-
-
-// [newest - 1]
 package com.project.student_management_system.controller;
 
 import com.project.student_management_system.model.Subject;
@@ -63,5 +28,10 @@ public class SubjectController {
     @DeleteMapping("/{id}")
     public void deleteSubject(@PathVariable String id) {
         subjectRepository.deleteById(id);
+    }
+
+    @GetMapping("/teacher")
+    public List<Subject> getSubjectsByTeacherEmail(@RequestParam String email) {
+        return subjectRepository.findByTeacherEmail(email);
     }
 }
--- src/main/java/com/project/student_management_system/repository/SubjectRepository.java
+++ src/main/java/com/project/student_management_system/repository/SubjectRepository.java
@@ -1,15 +1,3 @@
-//package com.project.student_management_system.repository;
-//
-//import com.project.student_management_system.model.Subject;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface SubjectRepository extends MongoRepository<Subject, String> {
-//}
-
-
-// [newest - 2]
 package com.project.student_management_system.repository;
 
 import com.project.student_management_system.model.Subject;
@@ -18,5 +6,6 @@ import java.util.List;
 
 public interface SubjectRepository extends MongoRepository<Subject, String> {
     List<Subject> findByGradeName(String gradeName); // Update this method
+    List<Subject> findByTeacherEmail(String teacherEmail);
 }
 
